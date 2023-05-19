6a43dcf99efc255284117e6e2fcdc6a628f7cf42
yneskladovae <yneskladovae27@gmail.com>
2023-05-19 23:27:33 +0300

    test new title

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -11,15 +11,24 @@ export const Projects = () => {
             {/*<h2 className={'projects__title'}>Featured Projects</h2>*/}
             <div className={'separatorWaveTop'}>
                 <img src={sectionSeparatorWave} alt="Section separator"/>
-                {/*<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">*/}
-                {/*    <path fill="#070422" fillOpacity="1"*/}
-                {/*          d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">*/}
-                {/*    </path>*/}
-                {/*</svg>*/}
             </div>
             <div className={'projects__items'}>
                 <div className={`${StyleContainer.container} projects__container`}>
-                    <h2 className={'projects__items__title'}>Projects</h2>
+                    {/*<h2 className={'projects__items__title'}>Projects</h2>*/}
+                    {/*<div className="position-relative d-flex text-center mb-5">*/}
+                    {/*    <h2 className="text-24  text-uppercase fw-600 w-100 mb-0 text-muted opacity-1">Portfolio</h2>*/}
+                    {/*    <p className="text-9 text-dark fw-600 position-absolute w-100 align-self-center lh-base mb-0 text-white">*/}
+                    {/*        Projects*/}
+                    {/*        <span className="heading-separator-line border-bottom border-3 border-primary d-block mx-auto"></span>*/}
+                    {/*    </p>*/}
+                    {/*</div>*/}
+                    <div className="position-relative d-flex text-center mb-5">
+                        <h2 className="text-24  text-uppercase fw-600 w-100 mb-0 text-muted opacity-1">Abilities</h2>
+                        <p className="text-9 text-dark fw-600 position-absolute w-100 align-self-center lh-base mb-0 text-white">
+                            Skills
+                            <span className="heading-separator-line border-bottom border-3 border-primary d-block mx-auto"></span>
+                        </p>
+                    </div>
                     <div className={'projects__items__projects'}>
                         {MyProjectsData.map(project => {
                             return <Project
@@ -34,10 +43,6 @@ export const Projects = () => {
                 </div>
             </div>
             <div className={'separatorWaveBottom'}>
-                {/*<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style={{transform: 'rotate(0.5turn)'}}>*/}
-                {/*    <path fill="#070422" fill-opacity="1"*/}
-                {/*          d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>*/}
-                {/*</svg>*/}
                 <img src={sectionSeparatorWave} alt="Section separator" style={{transform: 'rotate(0.5turn)'}}/>
             </div>
         </div>
